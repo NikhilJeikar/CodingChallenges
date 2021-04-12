081929c89df426e61211cef7d4e7ac5f43d87057
NikhilJeikar <nikhiljeikar@gmail.com>
2021-04-12 11:04:14 +0530

    Initial Commit

---
+++ Google Code Jam 2021/Qualification Round/MoonsAndUmberla.py
@@ -0,0 +1,71 @@
+TestCases = int(input())
+
+for TestCase in range(TestCases):
+    Prices = input()
+    Prices = Prices.split(' ')
+    X = int(Prices[0])
+    Y = int(Prices[1])
+    String = []
+    Cost = 0
+    for i in Prices[2]:
+        String.append(i)
+    Out = (0, 0)
+    for Char in range(len(String)):
+        # print(Char,len(String))
+        Prev = ""
+        Next = ""
+        if String[Char] == "?":
+            if Char == len(String) - 1:
+                Prev = String[Char - 1]
+            elif Char == 0:
+                Next = String[Char + 1]
+            else:
+                Prev = String[Char - 1]
+                Next = String[Char + 1]
+
+            Current = ""
+            if Prev == "J":
+                if Next == "J":
+                    Current = "J"
+                elif Next == "C":
+                    Current = "J"
+                elif Next == "?":
+                    Current = Prev
+                else:
+                    Current = Prev
+            elif Prev == "C":
+                if Next == "J":
+                    Current = "C"
+                elif Next == "C":
+                    Current = "C"
+                elif Next == "?":
+                    Current = Prev
+                else:
+                    Current = Prev
+            elif Prev == "":
+                x, y = Out
+                Out = (x, Char)
+                if Next != "?":
+                    XR, YR = Out
+                    for i in range(YR + 1):
+                        Current = Next
+                        String[i] = Current
+
+            String[Char] = Current
+
+    Prev = String[0]
+    for Char in String[1:]:
+        if Prev == "J" and Char == "C":
+            Cost = Cost + Y
+        elif Prev == "C" and Char == "J":
+            Cost = Cost + X
+        else:
+            pass
+        Prev = Char
+
+    print(f"Case #{TestCase+1}: {Cost}")
+
+"""
+Test Case 1 and 2 solved 
+Test case 3 pending
+"""
---
+++ Google Code Jam 2021/Qualification Round/ReverSort.py
@@ -0,0 +1,32 @@
+TestCases = int(input())
+
+for TestCase in range(TestCases):
+    N = int(input())
+    Inputs = input()
+    Inputs = [int(Input) for Input in Inputs.split(' ')]
+    Cost = 0
+    for i in range(0,len(Inputs) - 1):
+        Min = min(Inputs[i:])
+        Min = Inputs.index(Min)
+        Buffer = Inputs[i:Min + 1]
+        Buffer.reverse()
+        # print("Buffer" + str(Buffer))
+        Inputs = Inputs[:i] + Buffer + Inputs[Min + 1:]
+        # print(Inputs)
+        Cost = Cost + 1 + Min - i
+    print(f"Case #{TestCase+1}: {Cost}")
+
+"""
+Test case
+3
+4
+4 2 1 3
+2
+1 2
+7
+7 6 5 4 3 2 1
+
+1
+4
+4 2 1 3
+"""
